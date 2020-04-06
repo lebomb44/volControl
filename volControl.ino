18d4696590790af0188b1345467a23abc1de1ecb
LeBomb <cambon.olivier@gmail.com>
2020-04-06 23:55:59 +0200

    Add CnC interfaces

--- volControl.ino
+++ volControl.ino
@@ -3,7 +3,7 @@
  *  USING:
  *   ARDULIBS 1.1.0
  */
-#include <Cmd.h>
+#include <CnC.h>
 #include <IR.h>
 #include <SPI.h>
 #include <McpDigitalPot.h>
@@ -22,48 +22,86 @@
  * *****************************
  */
 IR ir;
-uint16_t relayToggleTimeout = 0;
-#define RELAYTOGGLE_TIMEOUT_START 1000
+uint32_t relayToggleTimeout = 0;
+#define RELAYTOGGLE_TIMEOUT_START 10000
 #define VOLUME_CHANNEL 0
 #define BALANCE_CHANNEL 1
 McpDigitalPot Mcp4261 = McpDigitalPot( MCP4261_SLAVE_SELECT_PIN, 50000.0, 0.0 );
 
-/* *****************************
- *  Debug Macros
- * *****************************
- */
-bool volControl_printIsEnabled = true;
-#define VOLCONTROL_PRINT(m) if(true == volControl_printIsEnabled) { m }
+uint32_t previousTime_1s = 0;
+uint32_t currentTime = 0;
+
+const char nodeName[] PROGMEM = "volcontrol";
+const char sepName[] PROGMEM = " ";
+const char hkName[] PROGMEM = "val";
+const char cmdGetName[] PROGMEM = "get";
+const char cmdSetName[] PROGMEM = "set";
+
+const char pingName[] PROGMEM = "ping";
+const char volName[] PROGMEM = "vol";
+const char volUpName[] PROGMEM = "volup";
+const char volDownName[] PROGMEM = "voldown";
+const char volMuteName[] PROGMEM = "volmute";
+const char volMaxName[] PROGMEM = "volmax";
+const char balName[] PROGMEM = "bal";
+const char balLeftName[] PROGMEM = "balleft";
+const char balRightName[] PROGMEM = "balright";
+const char onName[] PROGMEM = "on";
+const char offName[] PROGMEM = "off";
+const char powerName[] PROGMEM = "power";
+const char debugName[] PROGMEM = "debug";
 
 /* *****************************
  *  Command lines functions
  * *****************************
  */
-void redON (int arg_cnt, char **args) { digitalWrite(RED_LIGHT_PIN , HIGH); VOLCONTROL_PRINT(Serial.println("Red light ON");) }
-void redOFF (int arg_cnt, char **args) { digitalWrite(RED_LIGHT_PIN , LOW); VOLCONTROL_PRINT(Serial.println("Red light OFF");) }
-void relayTOGGLE (int arg_cnt, char **args) {
-  if(LOW == digitalRead(RELAY_PIN)) { if(0 == relayToggleTimeout) { digitalWrite(RELAY_PIN , HIGH); VOLCONTROL_PRINT(Serial.println("Relay ON");) } }
-  else { if(0 == relayToggleTimeout) { digitalWrite(RELAY_PIN , LOW); VOLCONTROL_PRINT(Serial.println("Relay OFF");) } }
+void ping_cmdGet(int arg_cnt, char **args) { cnc_print_cmdGet_u32(pingName, millis()); }
+void vol_cmdSet (int arg_cnt, char **args) {
+  if(4 == arg_cnt)
+  {
+    unsigned long volPos = strtoul(args[3], NULL, 10);
+    if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(volPos)); }
+    cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Volume "); cnc_Serial_get()->println(Mcp4261.getPosition(VOLUME_CHANNEL)); cnc_Serial_get()->flush();
+  }
+}
+void volUp_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(((int)Mcp4261.getPosition(VOLUME_CHANNEL))-1)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Volume UP "); cnc_Serial_get()->println(Mcp4261.getPosition(VOLUME_CHANNEL)); cnc_Serial_get()->flush();
+}
+void volDown_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(Mcp4261.getPosition(VOLUME_CHANNEL)+1)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Volume DOWN "); cnc_Serial_get()->println(Mcp4261.getPosition(VOLUME_CHANNEL)); cnc_Serial_get()->flush();
+}
+void volMute_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(256)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Volume MUTE "); cnc_Serial_get()->println(Mcp4261.getPosition(VOLUME_CHANNEL)); cnc_Serial_get()->flush();
+}
+void volMax_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(0)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Volume MAX "); cnc_Serial_get()->println(Mcp4261.getPosition(VOLUME_CHANNEL)); cnc_Serial_get()->flush();
+}
+void balLeft_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(BALANCE_CHANNEL, constrainPos(Mcp4261.getPosition(BALANCE_CHANNEL)+1)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Balance Left "); cnc_Serial_get()->println(Mcp4261.getPosition(BALANCE_CHANNEL)); cnc_Serial_get()->flush();
+}
+void balRight_cmdSet (int arg_cnt, char **args) {
+  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(BALANCE_CHANNEL, constrainPos(((int)Mcp4261.getPosition(BALANCE_CHANNEL))-1)); }
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Balance Right "); cnc_Serial_get()->println(Mcp4261.getPosition(BALANCE_CHANNEL)); cnc_Serial_get()->flush();
+}
+void on_cmdSet (int arg_cnt, char **args) {
+  digitalWrite(RELAY_PIN , HIGH); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Relay ON"); cnc_Serial_get()->flush();
+}
+void off_cmdSet (int arg_cnt, char **args) {
+  digitalWrite(RELAY_PIN , LOW); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Relay OFF"); cnc_Serial_get()->flush();
+}
+void relayTOGGLE(void) {
+  if(LOW == digitalRead(RELAY_PIN)) { if(0 == relayToggleTimeout) { digitalWrite(RELAY_PIN , HIGH); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Relay ON"); cnc_Serial_get()->flush(); } }
+  else { if(0 == relayToggleTimeout) { digitalWrite(RELAY_PIN , LOW); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Relay OFF"); cnc_Serial_get()->flush(); } }
   relayToggleTimeout = RELAYTOGGLE_TIMEOUT_START;
 }
-void volUp (int arg_cnt, char **args) {
-  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(((int)Mcp4261.getPosition(VOLUME_CHANNEL))-1)); }
-  VOLCONTROL_PRINT(Serial.print("Volume UP: "); Serial.println(Mcp4261.getPosition(VOLUME_CHANNEL));)
-}
-void volDown (int arg_cnt, char **args) {
-  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(VOLUME_CHANNEL, constrainPos(Mcp4261.getPosition(VOLUME_CHANNEL)+1)); }
-  VOLCONTROL_PRINT(Serial.print("Volume DOWN: "); Serial.println(Mcp4261.getPosition(VOLUME_CHANNEL));)
-}
-void balLeft (int arg_cnt, char **args) {
-  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(BALANCE_CHANNEL, constrainPos(Mcp4261.getPosition(BALANCE_CHANNEL)+1)); }
-  VOLCONTROL_PRINT(Serial.print("Balance Left: "); Serial.println(Mcp4261.getPosition(BALANCE_CHANNEL));)
-}
-void balRight (int arg_cnt, char **args) {
-  if(HIGH == digitalRead(RELAY_PIN)) { Mcp4261.setPosition(BALANCE_CHANNEL, constrainPos(((int)Mcp4261.getPosition(BALANCE_CHANNEL))-1)); }
-  VOLCONTROL_PRINT(Serial.print("Balance Right: "); Serial.println(Mcp4261.getPosition(BALANCE_CHANNEL));)
-}
-void volControlEnablePrint(int arg_cnt, char **args) { volControl_printIsEnabled = true; Serial.println("volControl print enabled"); }
-void volControlDisablePrint(int arg_cnt, char **args) { volControl_printIsEnabled = false; Serial.println("volControl print disabled"); }
+
+void redON (void) { digitalWrite(RED_LIGHT_PIN , HIGH); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Red light ON"); cnc_Serial_get()->flush(); }
+void redOFF(void) { digitalWrite(RED_LIGHT_PIN , LOW); cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Red light OFF"); cnc_Serial_get()->flush(); }
 
 unsigned int constrainPos(int pos) { return constrain(pos, 0, 256); }
 
@@ -77,64 +115,76 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 
-  redON(0, NULL);
+  cncInit(nodeName, &Serial);
+  cnc_Serial_get()->begin(115200);
+  cnc_hkName_set(hkName);
+  cnc_cmdGetName_set(cmdGetName);
+  cnc_cmdSetName_set(cmdSetName);
+  cnc_sepName_set(sepName);
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Starting..."); cnc_Serial_get()->flush();
 
-  Serial.begin(115200);
-  Serial.println("volControl Starting...");
+  redON();
 
   SPI.begin();
   Mcp4261.setPosition(VOLUME_CHANNEL, 250);
   Mcp4261.setPosition(BALANCE_CHANNEL, 128);
 
-  cmdInit();
-/*
-  cmdAdd("enablePrint", "Enable print", volControlEnablePrint);
-  cmdAdd("disablePrint", "Disable print", volControlDisablePrint);
-  cmdAdd("redON", "Red light ON", redON);
-  cmdAdd("redOFF", "Red light OFF", redOFF);
-*/
-  cmdAdd("relayTOGGLE", "Toggle relay", relayTOGGLE);
-  cmdAdd("volUp", "Volume UP", volUp);
-  cmdAdd("volDown", "Volume DOWN", volDown);
-  cmdAdd("balLeft", "Balance Left", balLeft);
-  cmdAdd("balRight", "Balance Right", balRight);
-  cmdAdd("help", "List commands", cmdList);
+  cnc_cmdGet_Add(pingName, ping_cmdGet);
+  cnc_cmdSet_Add(volName, vol_cmdSet);
+  cnc_cmdSet_Add(volUpName, volUp_cmdSet);
+  cnc_cmdSet_Add(volDownName, volDown_cmdSet);
+  cnc_cmdSet_Add(volMuteName, volMute_cmdSet);
+  cnc_cmdSet_Add(volMaxName, volMax_cmdSet);
+  cnc_cmdSet_Add(balLeftName, balLeft_cmdSet);
+  cnc_cmdSet_Add(balRightName, balRight_cmdSet);
+  cnc_cmdSet_Add(onName, on_cmdSet);
+  cnc_cmdSet_Add(offName, off_cmdSet);
 
-  Serial.println("volControl Init done");
+  redOFF();
+  previousTime_1s = millis();
 
-  redOFF(0, NULL);
+  cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->println("Started"); cnc_Serial_get()->flush();
 }
 
 void loop() {
+  currentTime = millis(); cncPoll();
+  /* HK @ 1.0Hz */
+  if((uint32_t)(currentTime - previousTime_1s) >= 1000) {
+    cnc_print_hk_bool(powerName, digitalRead(RELAY_PIN)); cncPoll(); cncPoll();
+    cnc_print_hk_u32(volName, Mcp4261.getPosition(VOLUME_CHANNEL)); cncPoll();
+    cnc_print_hk_u32(balName, Mcp4261.getPosition(BALANCE_CHANNEL)); cncPoll();
+    previousTime_1s = currentTime;
+  }
+
   ir.run();
   if(true == ir.rxSamsungCodeIsReady()) {
-    VOLCONTROL_PRINT( Serial.print(ir.rxGetSamsungCode(), HEX);Serial.print(" : "); )
-    VOLCONTROL_PRINT( Serial.print(ir.rxGetSamsungManufacturer(), HEX);Serial.print("-"); )
-    VOLCONTROL_PRINT( Serial.print(ir.rxGetSamsungData(), HEX);Serial.println(); )
+    cnc_print_cmdGet_tbd(debugName);
+    cnc_Serial_get()->print(ir.rxGetSamsungCode(), HEX); cnc_Serial_get()->print(" : ");
+    cnc_Serial_get()->print(ir.rxGetSamsungManufacturer(), HEX); cnc_Serial_get()->print("-");
+    cnc_Serial_get()->print(ir.rxGetSamsungData(), HEX); cnc_Serial_get()->println(); cnc_Serial_get()->flush();
     //delay(1);
     ir.purge();
     /* Check the authorized codes */
     if(0xF1F0 == ir.rxGetSamsungManufacturer()) {
-      redON(0, NULL);
+      redON();
       if(0x0FF1 == ir.rxGetSamsungData()) { // A
-        volUp(0, NULL);
+        volUp_cmdSet(0, NULL);
       }
       else if(0x17E9 == ir.rxGetSamsungData()) { // B
-        volDown(0, NULL);
+        volDown_cmdSet(0, NULL);
       }
       else if(0x1FE1 == ir.rxGetSamsungData()) { // C
-        relayTOGGLE(0, NULL);
+        relayTOGGLE();
       }
-      else { VOLCONTROL_PRINT( Serial.print("Samsung command unknown: "); Serial.println(ir.rxGetSamsungData(), HEX); ) }
-      redOFF(0, NULL);
+      else { cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Samsung command unknown "); cnc_Serial_get()->println(ir.rxGetSamsungData(), HEX); cnc_Serial_get()->flush(); }
+      redOFF();
     }
-    else { VOLCONTROL_PRINT( Serial.print("Samsung manufacturer unknown: "); Serial.println(ir.rxGetSamsungManufacturer(), HEX); ) }
+    else { cnc_print_cmdGet_tbd(debugName); cnc_Serial_get()->print("Samsung manufacturer unknown "); cnc_Serial_get()->println(ir.rxGetSamsungManufacturer(), HEX); cnc_Serial_get()->flush(); }
     ir.rxSamsungRelease();
   }
 
   /* Poll for new command line */
-  cmdPoll();
-  delay(1);
+  cncPoll();
+  //delay(1);
   if(0 < relayToggleTimeout) { relayToggleTimeout--; }
 }
-
